4f8e30aed690a03408f1f99735e750632897624e
Leonardo Pereira <leonardo_human@hotmail.com>
2023-08-23 11:39:07 -0300

    create movieRating table

--- src/database/knex/migrations/20230823143057_movieNotes.js
+++ src/database/knex/migrations/20230823143057_movieNotes.js
@@ -1,7 +1,8 @@
-exports.up = knex => knex.schema.createTable("notes", table => {
+exports.up = knex => knex.schema.createTable("movieNotes", table => {
     table.increments("id");
     table.text("title");
     table.text("description");
+    table.integer("rating");
     table.integer("user_id").references("id").inTable("users");
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
@@ -9,4 +10,4 @@ exports.up = knex => knex.schema.createTable("notes", table => {
 });
 
 
-exports.down = knex => knex.schema.dropTable("notes");
+exports.down = knex => knex.schema.dropTable("movieNotes");
